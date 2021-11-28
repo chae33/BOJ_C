5cc2eb1abb1882cc9db92f9add872ddcbc17fe85
chae33 <90388331+chae33@users.noreply.github.com>
2021-11-28 23:46:51 +0900

    Add files via upload

---
+++ 2884.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main() {
+	int hour, min;
+
+	scanf("%d %d", &hour, &min);
+
+	if (min >= 45)
+		printf("%d %d", hour, min - 5);
+	else {
+		if (hour == 0)
+			hour = 24;
+	}
+	printf("%d %d\n", hour - 1, min + 60 - 45);
+
+	return 0;
+}
